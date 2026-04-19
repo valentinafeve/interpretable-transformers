049ccd9cac9df9cde7d8684198a82555fad00e5a
Vale <valentinafeve@gmail.com>
2026-04-19 17:56:30 -0500

    :penguin: Training a model for a disperse attention

---
+++ experiments.py
@@ -0,0 +1,49 @@
+import os
+import json
+import torch
+from model import ViTForClassfication
+
+def save_experiment(experiment_name, config, model, data, base_dir="experiments"):
+    """"
+    Save the experiment results, including the config, the metrics and the model.    
+    """
+    outdir = os.path.join(base_dir, experiment_name)
+    os.makedirs(outdir, exist_ok=True)
+
+    # Save the config
+    configfile = os.path.join(outdir, 'config.json')
+    with open(configfile, 'w') as f:
+        json.dump(config, f, sort_keys=True, indent=4)
+
+    # Save the metrics
+    jsonfile = os.path.join(outdir, 'metrics.json')
+    with open(jsonfile, 'w') as f:
+        json.dump(data, f, sort_keys=True, indent=4)
+
+    # Save the model
+    save_checkpoint(experiment_name, model, "final", base_dir=base_dir)
+
+
+def save_checkpoint(experiment_name, model, epoch, base_dir="experiments"):
+    outdir = os.path.join(base_dir, experiment_name)
+    os.makedirs(outdir, exist_ok=True)
+    cpfile = os.path.join(outdir, f'model_{epoch}.pt')
+    torch.save(model.state_dict(), cpfile)
+
+
+def load_experiment(experiment_name, checkpoint_name="model_final.pt", base_dir="experiments"):
+    outdir = os.path.join(base_dir, experiment_name)
+    # Load the config
+    configfile = os.path.join(outdir, 'config.json')
+    with open(configfile, 'r') as f:
+        config = json.load(f)
+    # Load the metrics
+    jsonfile = os.path.join(outdir, 'metrics.json')
+    with open(jsonfile, 'r') as f:
+        data = json.load(f)
+
+    # Load the model
+    model = ViTForClassfication(config)
+    cpfile = os.path.join(outdir, checkpoint_name)
+    model.load_state_dict(torch.load(cpfile))
+    return config, model, data
